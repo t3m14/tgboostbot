1281c0f233f2d363a33a3be4c97e0dacc4144fe3
t3m14 <artemon.m174@gmail.com>
2022-11-18 17:51:47 +0300

    Просмотры добавляются. Баг с добавлением просмотров исправлен.

--- src/user/app.py
+++ src/user/app.py
@@ -1,37 +1,37 @@
 from utils.load import Config
 from pyrogram.raw.functions.messages import GetMessagesViews
 from pyrogram import filters, idle
+import asyncio
 class App():
     def __init__(self) -> None:
         self.config = Config()
         self.config.load()
         
- 
+    async def view_last_messsages(self, app, link):
+        channels_msgs = []
+        channel = await app.get_chat(chat_id=link.split("/")[-1] if "/" in link else link)
+        async for msg in app.get_chat_history(chat_id=channel.id,
+                                                limit=10):
+            channels_msgs.append(msg.id)
+
+        await app.invoke(GetMessagesViews(
+            peer=await app.resolve_peer(channel.id),
+            id=channels_msgs,
+            increment=True
+        )
+        )
     async def start(self):
+        tasks = []
         for app in self.config.apps:
             await app.start()
-            await self.main(app)
-            await idle()
+            print("started")
+            
+            @app.on_message(filters.chat("testing1441"))
+            async def get_post_and_put_to_queue(client, message):
+                print("POST GETTED" + str(message.id))
+                
+        await idle()
         for app in self.config.apps:
             await app.stop()
-    async def main(self, app):
-        print("OK")
-        @app.on_message(filters.chat("https://t.me/testing1441".split("/")[-1]))
-        async def get_post_and_put_to_queue(client, message):
-            channels_msgs = []
-            try:
-                channel = await app.get_chat(chat_id="https://t.me/testing1441")
-                async for msg in app.get_chat_history(chat_id=channel.id,
-                                                        limit=10):
-                    channels_msgs.append(msg.id)
 
-                await app.invoke(GetMessagesViews(
-                    peer=await app.resolve_peer(channel.id),
-                    id=channels_msgs,
-                    increment=True
-                )
-                )
-                print("viewd")
-            except Exception as e:
-                print(e)
 
